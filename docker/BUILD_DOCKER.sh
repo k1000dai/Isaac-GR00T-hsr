6c8271c2d339480b5c98d30a2c4bcb0d11917203
k1000dai <chiyodakku1000@gmail.com>
2025-06-20 19:39:17 +0900

    docker files#

---
+++ docker/BUILD_DOCKER.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ -n "$DATASETS_PATH" ]; then
+  echo "DATASETS_PATH is set to '$DATASETS_PATH'. Mounting this path to the container as '/root/datasets'."
+else
+  export DATASETS_PATH="$PWD/../datasets"
+  echo "DATASETS_PATH is not set. Using default path: $DATASETS_PATH"
+fi
+
+docker-compose build
