88b73a320075be9de2a2bf9aa3cd335e6f3e66c4
k1000dai <chiyodakku1000@gmail.com>
2025-06-20 19:02:25 +0900

    fix video channels

--- gr00t/data/dataset.py
+++ gr00t/data/dataset.py
@@ -311,8 +311,8 @@ class LeRobotSingleDataset(Dataset):
                 channels = le_video_meta["shape"][le_video_meta["names"].index("channel")]
                 fps = le_video_meta["video_info"]["video.fps"]
             except (ValueError, KeyError):
-                # channels = le_video_meta["shape"][le_video_meta["names"].index("channels")]
-                channels = le_video_meta["info"]["video.channels"]
+                channels = le_video_meta["shape"][le_video_meta["names"].index("channels")]
+                # channels = le_video_meta["info"]["video.channels"]
                 fps = le_video_meta["info"]["video.fps"]
             simplified_modality_meta["video"][new_key] = {
                 "resolution": [width, height],
